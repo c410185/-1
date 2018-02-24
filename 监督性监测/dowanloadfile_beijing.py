3be1a8912b7af6eb8142df7b2121b8bc390f3df1
c410185@163.com <c410185@163.com>
2018-02-24 17:42:02 +0800

    北京下载完成，早年网页公告没有改

--- 监督性监测/dowanloadfile_beijing.py
+++ 监督性监测/dowanloadfile_beijing.py
@@ -15,58 +15,57 @@ def parse_url_to_html(url):
         response = requests.get(url, headers=headers)
         soup = BeautifulSoup(response.content, 'lxml')
         # 附件链接
-        files_tag = soup.find_all('a',href=re.compile("(xls|xlsx|doc|docx)$"))
+        files_tag = soup.find_all('a',href=re.compile("(xls|xlsx|doc|docx|pdf)$"))
         return files_tag
-
     except Exception as e:
-
         logging.error("解析错误", exc_info=True)
 
 def get_file(url_tail,file_name):
-    file_path_head = r'D:\IPE_download\jdxjc\浙江\\'
-    url_head = 'http://www.zjepb.gov.cn'
+    file_path_head = r'D:\IPE_download\jdxjc\北京\\'
+    url_head = 'http://www.bjepb.gov.cn'
     url = url_head + url_tail
     try:
-       response =  requests.get(url)
+        #因为要下载二进制流文件，将stream参数置为True stream=True
+       response =  requests.get(url,stream=True)
        if response.content:
-           file_path = file_path_head + file_name
+           file_path = file_path_head + file_name + '.pdf'
            with open(file_path, 'wb') as f:
-               f.write(response.content)
-               print('附件:{}下载成功'.format(file_name))
+               for chunk in response.iter_content(1024):
+                   f.write(chunk)
+           print('附件:{}下载成功'.format(file_name))
     except:
         print('附件下载失败{}'.format(file_name))
-
-url1 = 'http://www.zjepb.gov.cn/col/col1201434/index.html?uid=3969356&pageNum=1'
-url2 = 'http://www.zjepb.gov.cn/col/col1201434/index.html?uid=3969356&pageNum=2'
-url3 = 'http://www.zjepb.gov.cn/col/col1201434/index.html?uid=3969356&pageNum=3'
-url4 = 'http://www.zjepb.gov.cn/col/col1201434/index.html?uid=3969356&pageNum=4'
 
 def get_urls(url):
     response = requests.get(url)
     response.encoding = 'utf-8'
     soup = BeautifulSoup(response.text,'lxml')
-    files_tag = soup.find('div',id="3969356")
-    file_a = BeautifulSoup(files_tag.text, 'lxml')
-    file_as = file_a.find_all('a')
+    files_tag = soup.find('div', class_="h_wz_m")
+    #print('files_tag 长度：',len(files_tag))
+    # print(files_tag)
+    file_as = files_tag.find_all('a')
     return file_as
 
 if __name__ == '__main__':
     #下载单个页面中的信息
-    message_url = ['http://www.zjepb.gov.cn/art/2013/12/5/art_1201434_14784357.html',
-                   'http://www.zjepb.gov.cn/art/2013/11/22/art_1201434_14784363.html',
-                   'http://www.zjepb.gov.cn/art/2013/11/4/art_1201434_14784354.html',
-                   'http://www.zjepb.gov.cn/art/2013/5/15/art_1201434_14784351.html']
-    for i in message_url:
-        m = parse_url_to_html(i)
-        for i in m:
-            get_file(i['href'],i.text)
-
-
-    ## 获取单个页面信息后分页面自动下载
-    # m = get_urls(url2)
-    # print(len(m))
-    # for i in m:
-    #     sign_url = 'http://www.zjepb.gov.cn/' + i['href']
-    #     m = parse_url_to_html(sign_url)
+    # message_url = ['http://www.bjepb.gov.cn/bjhrb/xxgk/ywdt/hjjc/gkwryjdxjcjg/827653/index.html']
+    # for i in message_url:
+    #     m = parse_url_to_html(i)
     #     for i in m:
     #         get_file(i['href'],i.text)
+    # text_url = '/bjhrb/xxgk/ywdt/hjjc/gkwryjdxjcjg/827653/2018010908323370982.pdf'
+    # text_name = '2017年第四季度国控工业企业监督性监测结果汇总公开数据表-测试'
+    # get_file(text_url,text_name)
+
+    url1 = 'http://www.bjepb.gov.cn/bjhrb/ztzl/wryhjjgxx/wryjc/012cbce7-1.html'
+    url2 = 'http://www.bjepb.gov.cn/bjhrb/ztzl/wryhjjgxx/wryjc/012cbce7-2.html'
+    url3 = 'http://www.bjepb.gov.cn/bjhrb/ztzl/wryhjjgxx/wryjc/012cbce7-3.html'
+
+    # 获取单个页面信息后分页面自动下载
+    m = get_urls(url3)
+    print(len(m))
+    for i in m:
+        sign_url = 'http://www.bjepb.gov.cn' + i['href']
+        m = parse_url_to_html(sign_url)
+        for i in m:
+            get_file(i['href'],i.text)
